dcceb15d5bb28b157e4db79518be7e52fd11cde6
Al Mohaimin Farabi <almohaiminfarabi36@gmail.com>
2021-11-17 15:15:10 +0600

    footer added in explore page

--- src/Pages/Explore/Explore.js
+++ src/Pages/Explore/Explore.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Cars from "../Shared/Cars/Cars";
 import Navigation from "../Shared/Navigation/Navigation";
-
+import Footer from "../Shared/Footer/Footer";
 const Explore = () => {
   return (
     <>
       <Navigation></Navigation>
-
       <Cars>Cars Available</Cars>
+      <Footer></Footer>
     </>
   );
 };
